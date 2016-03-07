c408d22e1c7ba3ff23e1828a126b3106ec4acd74
Hango Milan <hmilancodecool@gmail.com>
2016-03-07 13:03:36 +0100

    Simple Java Login App.

---
+++ src/LoginServlet.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginServlet extends HttpServlet
+{
+	private static final long serialVersionUID = 9211568140687018262L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		String name = request.getParameter("userName");
+		String password = request.getParameter("passwd");
+
+		HttpSession session = request.getSession();
+		if (name.equals("milan") && password.equals("1234"))
+		{
+			if (session.getAttribute("userName") == null || !session.getAttribute("userName").equals(name))
+			{
+				out.print("Welcome " + name + "! <a href=\"ProfileServlet\">Go to your profile</a>");
+				session.setAttribute("userName", name);
+			} else
+			{
+				response.sendRedirect("ProfileServlet");
+			}
+		} else
+		{
+			out.print("<span class=\"error\">Bad username or password! Permission denied</span>");
+			request.getRequestDispatcher("index.html").include(request, response);
+		}
+		out.close();
+	}
+
+}
